02f7c1e7c08bb98a930ff23455e29e4e1541d243
Graziela <grazims26@gmail.com>
2019-08-19 20:38:13 -0700

    added name and removed extra comments

--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -1,56 +1,67 @@
-import { SVG_NS, SPEED } from '../settings';
+import { SVG_NS, SPEED } from "../settings";
 
-export default class Paddle { 
-    constructor(boardHeight, paddleWidth, paddleHeight, initialX, initialY, keyUp, keyDown) {
-        this.boardHeight = boardHeight;
-        this.paddleWidth = paddleWidth;
-        this.paddleHeight = paddleHeight;
-        this.x = initialX;
-        this.y = initialY;
-        this.score = 0;
-        this.speed = SPEED;
-        document.addEventListener("keydown", event => {
-            switch(event.key) {
-                case keyUp:
-                this.moveUp();
-                break;
-                case keyDown:
-                this.moveDown();
-                break;
-            }
-        });
-    }
-increaseScore() {
+export default class Paddle {
+  constructor(
+    boardHeight,
+    paddleWidth,
+    paddleHeight,
+    initialX,
+    initialY,
+    keyUp,
+    keyDown
+  ) {
+    this.boardHeight = boardHeight;
+    this.paddleWidth = paddleWidth;
+    this.paddleHeight = paddleHeight;
+    this.x = initialX;
+    this.y = initialY;
+    this.score = 0;
+    this.speed = SPEED;
+    document.addEventListener("keydown", event => {
+      switch (event.key) {
+        case keyUp:
+          this.moveUp();
+          break;
+        case keyDown:
+          this.moveDown();
+          break;
+      }
+    });
+  }
+  increaseScore() {
     this.score++;
-}
-getScore() {
+  }
+  getScore() {
     return this.score;
-}
-setSpeed(speed) {
+  }
+  setSpeed(speed) {
     this.speed = speed;
-}
-moveUp() {
+  }
+  moveUp() {
     this.y = Math.max(0, this.y - this.speed);
-}
-moveDown() {
-    this.y = Math.min(this.boardHeight - this.paddleHeight - this.speed, this.y + this.speed);
-}
-getCoordinates() {
+  }
+  moveDown() {
+    this.y = Math.min(
+      this.boardHeight - this.paddleHeight - this.speed,
+      this.y + this.speed
+    );
+  }
+  getCoordinates() {
     const walls = {
-        left: this.x,
-        top: this.y,
-        right: this.x + this.paddleWidth,
-        bottom: this.y + this.paddleHeight,
-    }
+      left: this.x,
+      top: this.y,
+      right: this.x + this.paddleWidth,
+      bottom: this.y + this.paddleHeight
+    };
     return walls;
-}
-render(svg) {
-      let rect = document.createElementNS(SVG_NS, 'rect');
-          rect.setAttributeNS(null, 'width', this.paddleWidth);
-          rect.setAttributeNS(null, 'height', this.paddleHeight);
-          rect.setAttributeNS(null, 'fill', 'yellow');
-          rect.setAttributeNS(null, "x", this.x);
-          rect.setAttributeNS(null, "y", this.y);
-          svg.appendChild(rect);
-}
+  }
+  render(svg) {
+    let rect = document.createElementNS(SVG_NS, "rect");
+    rect.setAttributeNS(null, "width", this.paddleWidth);
+    rect.setAttributeNS(null, "height", this.paddleHeight);
+    rect.setAttributeNS(null, "fill", "yellow");
+    rect.setAttributeNS(null, "x", this.x);
+    rect.setAttributeNS(null, "y", this.y);
+    svg.appendChild(rect);
+  }
 }
